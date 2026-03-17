11e42c610c1dc806e5f74f68969505d921364078
Aleksandra <aleksandra.kwasniewska@student.put.poznan.pl>
2026-03-17 14:53:44 +0100

    poprawione wyswietlanie

--- main.py
+++ main.py
@@ -133,14 +133,17 @@ if __name__ == '__main__':
     for apartment in manager.apartments.values():
         print(apartment.key, apartment.name, apartment.location, apartment.area_m2)
         for room in apartment.rooms.values():
+            print("Podstawowe informacje: ")
             print('  ', room.name, room.area_m2)
         
         for bill in manager.bills:
             if bill.apartment == apartment.key:
+                print("Koszta: ")
                 print('  ', bill.amount_pln, bill.date_due, bill.settlement_year, bill.settlement_month, bill.type)
 
     for tenant in manager.tenants.values():
         print(tenant.name, tenant.apartment, tenant.room, tenant.rent_pln, tenant.deposit_pln, tenant.date_agreement_from, tenant.date_agreement_to)
         for transfer in manager.transfers:
             if transfer.tenant == tenant.name:
+                print("Przelew: ")
                 print('  ', transfer.amount_pln, transfer.date, transfer.settlement_year, transfer.settlement_month)
